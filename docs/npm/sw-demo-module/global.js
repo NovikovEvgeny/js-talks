c0f2c5ab8ab2a3ad9e2bfbdd966d8cfe6eba2179
Evgeniy Novikov <epn94@mail.ru>
2019-12-15 18:59:42 +0300

    npm packages

---
+++ docs/npm/sw-demo-module/global.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+const sw = require('./index');
+
+sw.sayHi(process.argv[2]);
+
---
+++ docs/npm/sw-demo-module/index.js
@@ -0,0 +1,13 @@
+/**
+ * Prints "Hi, name!" where name is passed argument
+ *
+ * @param name name to print
+ * @returns void
+ */
+function sayHi(name) {
+  console.log(`Hi, ${name}`);
+}
+
+module.exports = {
+  sayHi,
+};
