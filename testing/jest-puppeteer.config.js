39cf7d994458bff776b98429084089bb31884d55
Evgeniy Novikov <epn94@mail.ru>
2020-02-09 18:25:25 +0300

    UI testing

---
+++ testing/jest-puppeteer.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  launch: {
+    dumpio: true,
+    headless: false,
+    defaultViewport: {
+      width: 1024,
+      height: 1000,
+    }
+  },
+  browser: 'chromium',
+  browserContext: 'default',
+};
